44a61726998165db7f6b58bd60a241392d5127ae
Marius Hein <marius.hein@netways.de>
2012-03-15 14:29:28 +0100

    * Created license information for all modules

--- lib/RTx/CreateLinkedTickets.pm
+++ lib/RTx/CreateLinkedTickets.pm
@@ -77,3 +77,27 @@ sub createTicketByConfiguration {
 }
 
 1;
+=pod
+
+=head1 NAME
+
+RTx::CreateLinkedTickets
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Marius Hein <marius.hein@netways.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by NETWAYS GmbH <info@netways.de>
+
+This is free software, licensed under:
+    GPL Version 2, June 1991
+
+=cut
+
+__END__
